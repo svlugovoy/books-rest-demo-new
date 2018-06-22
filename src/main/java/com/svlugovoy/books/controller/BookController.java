75b5593ca392a8000b944304d7662219401913d2
svlugovoy <svlugovoy@gmail.com>
2018-06-22 18:18:37 +0300

    added error handling (1st approach)

--- src/main/java/com/svlugovoy/books/controller/BookController.java
+++ src/main/java/com/svlugovoy/books/controller/BookController.java
@@ -2,11 +2,14 @@ package com.svlugovoy.books.controller;
 
 import com.svlugovoy.books.domain.Book;
 import com.svlugovoy.books.repository.BookRepository;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -23,11 +26,29 @@ public class BookController {
         return bookRepository.findAll();
     }
 
+//    @GetMapping(path = "/{id}",
+//            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
+//    public Book findBookById(@PathVariable String id) {
+//        return bookRepository.findById(Long.valueOf(id)).get();
+//    }
+
     @GetMapping(path = "/{id}",
             produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Book findBookById(@PathVariable String id) {
-        return bookRepository.findById(Long.valueOf(id)).get();
+    public ResponseEntity<Book> findBookById(@PathVariable String id) {
+
+        int bookId = NumberUtils.toInt(id);
+        if(bookId <= 0) {
+            return ResponseEntity.badRequest().build(); //400
+        }
+
+        Optional<Book> optional = bookRepository.findById((long) bookId);
+        if (!optional.isPresent()) {
+            return ResponseEntity.notFound().build(); //404
+        }
+
+        return ResponseEntity.ok(optional.get()); //200
     }
+
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE})
     public void createBook(@Valid @RequestBody Book book) {
